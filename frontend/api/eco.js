895c900ea5117ae0b0bff697f85f72b0b070d0a5
krzysztofzelman666-png <krzysztofzelman666@gmail.com>
2026-05-17 18:50:27 +0200

    fix: aktualizacja danych PSZOK – poprawne lokalizacje ZOMM Mysłowice
    - PSZOK Północ: ul. Miarki, 50.230722, 19.134722, Pon-Sob 8:00-18:00
    - PSZOK Południe: ul. Piaskowa, 50.201583, 19.177315, Pon-Sob 8:00-18:00

--- frontend/api/eco.js
+++ frontend/api/eco.js
@@ -1,23 +1,23 @@
 const ecoPoints = [
   {
     id: 1,
-    name: 'PSZOK Mysłowice Północ',
+    name: 'ZOMM Mysłowice - PSZOK Północ',
     type: 'PSZOK',
-    address: 'Karola Miarki 38, 41-400 Mysłowice',
-    hours: 'Pon-Pt 12:00-20:00, Sob 8:00-16:00',
-    phone: '32 222 22 18, 32 222 22 19',
-    accepts: 'Papier, metale, tworzywa sztuczne, szkło, odpady opakowaniowe wielomateriałowe, bioodpady, odpady niebezpieczne, przeterminowane leki i chemikalia, odpady medyczne (igły i strzykawki z domowego użytku), zużyte baterie i akumulatory, zużyty sprzęt elektryczny i elektroniczny (AGD, elektronika), meble i odpady wielkogabarytowe, zużyte opony, odpady budowlane i rozbiórkowe (gruz), tekstylia i odzież',
-    coordinates: { lat: 50.23073471730748, lng: 19.134723086506078 }
+    address: 'ul. Miarki, Mysłowice',
+    hours: 'Pon-Sob 8:00-18:00',
+    phone: '',
+    accepts: 'elektrośmieci, meble, gruz, opony, tekstylia',
+    coordinates: { lat: 50.230722, lng: 19.134722 }
   },
   {
     id: 2,
-    name: 'PSZOK Mysłowice Południe',
+    name: 'ZOMM Mysłowice - PSZOK Południe',
     type: 'PSZOK',
-    address: 'Piaskowa, 41-404 Mysłowice',
-    hours: 'Pon-Pt 8:00-16:00',
-    phone: '882 682 983',
-    accepts: 'Papier, metale, tworzywa sztuczne, szkło, odpady opakowaniowe wielomateriałowe, bioodpady, odpady niebezpieczne, przeterminowane leki i chemikalia, odpady medyczne (igły i strzykawki z domowego użytku), zużyte baterie i akumulatory, zużyty sprzęt elektryczny i elektroniczny (AGD, elektronika), meble i odpady wielkogabarytowe, zużyte opony, odpady budowlane i rozbiórkowe (gruz), tekstylia i odzież',
-    coordinates: { lat: 50.2015855831223, lng: 19.17731519815247 }
+    address: 'ul. Piaskowa, Mysłowice',
+    hours: 'Pon-Sob 8:00-18:00',
+    phone: '',
+    accepts: 'elektrośmieci, meble, gruz, opony, tekstylia',
+    coordinates: { lat: 50.201583, lng: 19.177315 }
   },
   {
     id: 3,
